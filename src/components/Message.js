f87195298fda3ee115a69beedac1568c4c46cf35
Braden Lawrence <b@bradenlawrence.com>
2020-03-09 17:56:22 -0400

    update message component to be a class with react lifecycle syntax

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,14 +1,31 @@
-import React from "react"
+import React, { Component } from "react"
 
-const Message = props => {
-  const clickCount = 0
+class Message extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: this.props.message,
+      clickCount: 0
+    }
 
-  return (
-    <div>
-      <h1>Message: {props.message}</h1>
-      <h1>Click Count: {clickCount}</h1>
-    </div>
-  )
+    this.incrementCount = this.incrementCount.bind(this)
+  }
+
+  incrementCount(event) {
+    event.preventDefault()
+    const newCount = this.state.clickCount + 1
+    this.setState({clickCount: newCount})
+  }
+
+  render() {
+
+    return(
+      <div onClick={this.incrementCount}>
+        <h1>Component Message: {this.state.message}</h1>
+        <h1>Component Click Count: {this.state.clickCount}</h1>
+      </div>
+    )
+  }
 }
 
 export default Message
